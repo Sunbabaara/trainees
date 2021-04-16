677e5035b6b3bfa88a2224a7e6dc504bd3b256fc
Sunbabaara <csunbabaara@gmail.com>
2021-04-16 16:09:34 +0000

    added a procfile

---
+++ controllers/traineesController.js
@@ -0,0 +1,57 @@
+const Trainee = require("../models/traineeSchema");
+
+//create trainee
+const createTrainee = async (req, res)=>{
+    const newTrainee= new Trainee(
+        {
+            name: req.body.name,
+            destination: req.body.destination,
+            email:req.body.email,
+            dob:  req.body.dob,
+        } )
+        await newTrainee.save()
+        res.status(201).json(newTrainee);
+
+};
+
+const getAllTrainees = async(req, res)=>{
+    const trainees = await Trainee.find();
+    res.json(trainees);
+}
+ 
+//get a trainee
+const getSingleTrainee = async (req, res)=>{
+      const trainee =await Trainee.findById(req.params._id)
+      res.json(trainee);
+
+};
+
+//update a trainee
+const  updateTrainee = async (req, res)=>{
+    const foundTrainee = await Trainee.findById(req.params._id)
+    if (foundTrainee) {
+        foundTrainee.name = req.body.name,
+        foundTrainee.destination = req.body.destination,
+        foundTrainee.email = req.body.email,
+        foundTrainee.dob = req.body.dob
+
+        const updatedTrainee = await foundTrainee.save();
+        res.json({updatedTrainee})
+    }
+}
+
+//delete a trainee
+const deleteTrainee = async (req, res)=>{
+    const foundTrainee = await Trainee.findById(req.params._id)
+    if (foundTrainee) {
+        foundTrainee.remove()
+        res.json({msg:`${foundTrainee.name} removed`})
+    } else {
+        res.status(404).json({error:"Trainee not found"})
+    }
+}
+
+module.exports ={createTrainee,
+     getAllTrainees,
+      getSingleTrainee,
+       updateTrainee, deleteTrainee};
